2cd03e13364b77175c790b2768e5b057ef291adf
Maxine <misscmax@hotmail.com>
2023-06-27 12:19:41 +0100

    file

---
+++ selection_part1.py
@@ -0,0 +1,8 @@
+age = int(input("Please enter your age: "))
+
+if age >= 18:
+    print("You are in catagory 'A'")
+elif age >= 16:
+    print("You are in catagory 'B'")
+else:
+    print("You are in catagory 'C'")
